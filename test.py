31015e35f8cc2417f967a0a26048d048a119fa90
blaytn <blaytn@gmail.com>
2024-07-31 18:20:59 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import plotly.express as px
-import pandas as pd
-
-t = yf.Ticker('AAPL')
-t_data = t.history(period='1d', interval='1m')
-
-data = pd.DataFrame(t_data)
-print(data)
